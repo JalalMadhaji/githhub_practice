fece01593b5f146ac2e3ec1a435aaa401947c8c6
JalalMadhaji <jalal.almadhaji90@gmail.com>
2019-11-11 23:02:29 +0300

    fix cards and nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
+let products = document.getElementById("products");
 const getallproducts = async () => {
-    const base = "http://localhost:3000/product";
-    const response = await fetch(base);
-    const data = await response.json();
-   
-    return data;
-    
-  };
+  const base = "http://localhost:3000/product";
+  const response = await fetch(base);
+  const data = await response.json();
+
+  return data;
+};
+
+getallproducts().then(data => {
+  data.forEach(element => {
+    let div = document.createElement("div");
+    let name = document.createElement("h4");
+    let price = document.createElement("span");
+    let para = document.createElement("p");
+    let img = document.createElement("img");
+    let button = document.createElement("button");
+    img.src = "img/pictures/" + element.server_image_path;
+    button.innerHTML = "Buy Now";
+    name.innerHTML = element.Name.substring(1, 20);
+    price.innerHTML = "$" + element.price;
+    para.innerHTML = element.description.substring(1, 20);
+    div.appendChild(img);
+    div.appendChild(name);
+    div.appendChild(para);
+    div.appendChild(price);
+    div.appendChild(button);
+    products.appendChild(div);
+  });
+});
