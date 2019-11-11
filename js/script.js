1ca562735b0cde18f0f1c3eb34c4e8afd0fd79b4
EmanAlzariey <eman_alzariey@yahoo.com>
2019-11-11 17:10:53 +0300

    add pic with fetch

--- js/script.js
+++ js/script.js
@@ -17,9 +17,12 @@ fetch("http://localhost:3000/product")
         let u1 = document.createElement("div")
         u1.className = "u1 flex1"
         let img = document.createElement("img")
-        img.setAttribute("src", element.server_image_path);
+        let h1 = document.createElement("h1");
+        h1.innerHTML = element.Name;
+        img.setAttribute("src", "./img/pictures/" + element.server_image_path);
         u1.appendChild(img)
         up.appendChild(u1)
+        u1.appendChild(h1)
 
 
       }
