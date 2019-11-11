1268372525244e6284eefb4489c6f0150901771f
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-11-11 16:18:34 +0300

    css update

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ const getallproducts = async () => {
     const base = "http://localhost:3000/product";
     const response = await fetch(base);
     const data = await response.json();
+   
     return data;
+    
   };
