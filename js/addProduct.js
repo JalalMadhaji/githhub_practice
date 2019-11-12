b5d8454b97237549eb53526effda12e4f76bec9b
AISHA-PRO <aisha1rashed@gmail.com>
2019-11-12 20:45:03 +0300

    add new product in json file
    edit ID in json file to id

---
+++ js/addProduct.js
@@ -0,0 +1,36 @@
+
+document.getElementById("addProductForm").addEventListener("submit",e=>{
+    e.preventDefault();
+    let productName = document.getElementById("productName");
+    let price = document.getElementById("price");
+    let description = document.getElementById("description");
+    let imgName = document.getElementById("imgName");
+
+    console.log(productName.value);
+    console.log(price.value);
+    fetch("http://localhost:3000/product",{
+        method: 'post',
+        body: JSON.stringify({
+            
+            Name : productName.value,
+            price : price.value,
+            description : description.value,
+            server_image_path : `${imgName.value}.jpg`,
+
+        }),
+        headers: {
+            Accept: "application/json, text/plain, /",
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response=>response.json())
+    .then(json => {
+        console.log(json);
+        console.log(json.body);
+        console.log(json.headers);
+    })
+    // .catch(e => {
+    //     console.log("error");
+    // })
+
+});
